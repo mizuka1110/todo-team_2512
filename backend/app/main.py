628cd16a73026a426eb29a609c1efa300ec8bad1
山崎瑞佳 <nazoneko1110@gmail.com>
2025-12-14 16:59:34 +0900

    task CRUD多分できた

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,32 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.database import SessionLocal
 from app.core.security import verify_token
 from app.firebase_admin import *  # Firebase Admin 初期化
 
+from app.crud.task_crud import (
+    get_tasks_by_uid,
+    create_task_by_uid,
+    delete_task_by_uid,
+)
+
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class TaskCreate(BaseModel):
+    title: str
+    description: str | None = None
 
 # =========================
 # DB セッション依存関数
@@ -45,10 +66,28 @@ def protected(user=Depends(verify_token)):
         "uid": user["uid"],
         "email": user.get("email")
     }
-
+# データ取得
 @app.get("/tasks")
 def get_tasks(user=Depends(verify_token)):
     uid = user["uid"]
-    tasks = get_tasks_by_uid(uid)      # ←さるちゃんへ、ここからDBとの接続、多分
+    tasks = get_tasks_by_uid(uid) 
     return tasks
+
+# データ削除
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int, user=Depends(verify_token)):
+    uid = user["uid"]
+    delete_task_by_uid(task_id, uid)
+    return {"status": "ok"}
+
+# データ作成
+@app.post("/tasks")
+def create_task(
+    task: TaskCreate,
+    user=Depends(verify_token)
+):
+    uid = user["uid"]
+    new_task = create_task_by_uid(uid, task)
+    return new_task
+
 
